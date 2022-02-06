e4d314f76a030e60e1c7732d374603b592721814
Charlie Wright <cw806@cam.ac.uk>
2022-02-06 21:21:12 +0000

    Task1D.py

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -6,4 +6,24 @@ geographical data.
 
 """
 
-from .utils import sorted_by_key  # noqa
+#from floodsystem.utils import sorted_by_key  # noqa
+#1d
+def rivers_with_station(stations):
+    rivers=[]
+    for station in stations:
+        if station.river not in rivers:
+            rivers.append(station.river)
+    return rivers
+
+def stations_by_river(stations):
+    river_dict = {}
+    rivers = rivers_with_station(stations)
+    #key is name, object is a list of stations
+    for river in rivers:
+        key = river
+        object= []
+        for station in stations:
+            if station.river == river:
+                object.append(station)
+        river_dict[key]=object
+    return river_dict
