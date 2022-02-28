76b83134bf6ec152e05ed83daeb07b7ecfa810cc
Charlie Wright <cw806@cam.ac.uk>
2022-02-28 00:01:17 +0000

    commit

--- Task2D.py
+++ Task2D.py
@@ -39,8 +39,7 @@ def run():
 
     # Fetch data over past 2 days
     dt = 2
-    dates, levels = fetch_measure_levels(
-        station_cam.measure_id, dt=datetime.timedelta(days=dt))
+    dates, levels = fetch_measure_levels(station_cam.measure_id, dt=datetime.timedelta(days=dt))
 
     # Print level history
     for date, level in zip(dates, levels):
--- floodsystem/Task2G.py
+++ floodsystem/Task2G.py
@@ -1,24 +1,27 @@
+from dataclasses import replace
+import datetime
+from os import remove
+import matplotlib.pyplot as plt
+from pytest import skip
 from floodsystem.datafetcher import fetch_measure_levels
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_highest_relative_level
-from floodsystem.plot2 import *
-import datetime
-import matplotlib.pyplot as plt
-from floodsystem.flood import *
-import numpy as py
+from floodsystem.plot import plot_water_levels
+import numpy as np
 
 
 def run():
-    stations=build_station_list()
+    stations = build_station_list()
     update_water_levels(stations)
-    N=6
-    dt=2
-    p =4
-    stations_N = (stations_highest_relative_level(stations, N))
+    N = 5
+    dt = 10
 
-    list=[]
+    stations_N = (stations_highest_relative_level(stations, N))
+    list = []
+
     for i in stations_N:
         list.append(i[0])
+
     list_1 = []
     for station in stations:
         if station.name in list:
@@ -29,8 +32,7 @@ def run():
         if len(dates) == 0:
             print(station.name, 'data error')
         else:
-            plot_water_level_with_fit(station, dates, levels, p)
-            #print()
+            return ((sum (levels))/958)
 if __name__ == "__main__":
-    print("*** Task 2F: CUED Part IA Flood Warning System ***")
+    print("*** Task 2G: CUED Part IA Flood Warning System ***")
     run()
